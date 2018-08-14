5b96152ccf25c3c4322d79b1c1eea1423c7ca684
chris <12405301@qq.com>
2018-08-15 00:17:41 +0800

    udpate

--- app/src/main/java/com/boyu/emove/login/LoginActivity.kt
+++ app/src/main/java/com/boyu/emove/login/LoginActivity.kt
@@ -2,12 +2,19 @@ package com.boyu.emove.login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.boyu.emove.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = LoginActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tv_code.setOnClickListener {
+            Log.d(TAG, "code")
+        }
     }
 }
