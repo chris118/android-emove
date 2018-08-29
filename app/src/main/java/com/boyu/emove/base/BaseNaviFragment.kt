8ce7e291aa2e768130476f0a5c5fdbdc142a5a46
xiaopeng <12405301@qq.com>
2018-08-29 22:03:15 +0800

    BaseNaviFragment

--- app/src/main/java/com/boyu/emove/base/BaseNaviFragment.kt
+++ app/src/main/java/com/boyu/emove/base/BaseNaviFragment.kt
@@ -1,5 +1,40 @@
 package com.boyu.emove.base
 
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.boyu.emove.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 /**
  * Created by chrisw on 2018/8/28.
  */
+abstract class BaseNaviFragment(): Fragment() {
+    abstract fun getTargetLayoutId(): Int
+
+    init {
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.menu_item_next, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.goodsFragment -> {
+                val navigation =  Navigation.findNavController(activity!!, R.id.nav_host_fragment)
+                navigation.navigate(getTargetLayoutId())
+
+//                NavigationUI.onNavDestinationSelected(item,
+//                        Navigation.findNavController(activity!!, R.id.nav_host_fragment))
+//                        || super.onOptionsItemSelected(item)
+
+                activity?.bnv_bottom_navigation?.visibility = View.GONE
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
--- app/src/main/java/com/boyu/emove/info/InfoFragment.kt
+++ app/src/main/java/com/boyu/emove/info/InfoFragment.kt
@@ -1,16 +1,14 @@
 package com.boyu.emove.info
 
 import android.os.Bundle
-import android.view.*
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation
 import com.boyu.emove.R
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.info_fragment.*
+import com.boyu.emove.base.BaseNaviFragment
 
-class InfoFragment : Fragment() {
-
+class InfoFragment : BaseNaviFragment() {
     companion object {
         fun newInstance() = InfoFragment()
     }
@@ -19,7 +17,7 @@ class InfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        setHasOptionsMenu(true)
+//        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.info_fragment, container, false)
     }
 
@@ -27,30 +25,31 @@ class InfoFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         viewModel = ViewModelProviders.of(this).get(InfoViewModel::class.java)
+    }
 
-        btn_go.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_infoFragment_to_goodsFragment))
+    override fun getTargetLayoutId(): Int {
+        return R.id.action_infoFragment_to_goodsFragment
     }
 
 
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.menu_item_next, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.goodsFragment -> {
-
-                val navi =  Navigation.findNavController(activity!!, R.id.nav_host_fragment)
-                navi.navigate(R.id.action_infoFragment_to_goodsFragment)
-
-//                NavigationUI.onNavDestinationSelected(item,
-//                        Navigation.findNavController(activity!!, R.id.nav_host_fragment))
-//                        || super.onOptionsItemSelected(item)
-
-                activity?.bnv_bottom_navigation?.visibility = View.GONE
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+//        inflater?.inflate(R.menu.menu_item_next, menu)
+//        super.onCreateOptionsMenu(menu, inflater)
+//    }
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        return when (item?.itemId) {
+//            R.id.goodsFragment -> {
+//                val navigation =  Navigation.findNavController(activity!!, R.id.nav_host_fragment)
+//                navigation.navigate(R.id.action_infoFragment_to_goodsFragment)
+//
+////                NavigationUI.onNavDestinationSelected(item,
+////                        Navigation.findNavController(activity!!, R.id.nav_host_fragment))
+////                        || super.onOptionsItemSelected(item)
+//
+//                activity?.bnv_bottom_navigation?.visibility = View.GONE
+//                return true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 }
